d242d2647beed9d20b9ee93c4c2de3d8b8955173
Dimash <dimashkenesov330@gmail.com>
2026-05-14 03:20:53 +0500

    2

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -5,9 +5,14 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import java.util.List;
 
 public interface ItemService {
+
     ItemDto create(Long userId, ItemDto itemDto);
+
     ItemDto update(Long userId, Long itemId, ItemDto itemDto);
+
     ItemDto getById(Long itemId);
+
     List<ItemDto> getAllByOwner(Long userId);
+
     List<ItemDto> search(String text);
 }
--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
+
     User save(User user);
+
     Optional<User> findById(Long id);
+
     List<User> findAll();
+
     User update(User user);
+
     void delete(Long id);
+
     boolean existsByEmail(String email);
 }
