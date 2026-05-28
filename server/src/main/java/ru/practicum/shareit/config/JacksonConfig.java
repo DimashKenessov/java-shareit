e32d30bb622fbe551a819b6fb06edb09849b78bc
Dimash <dimashkenesov330@gmail.com>
2026-05-28 18:16:38 +0500

    5th commit

--- server/src/main/java/ru/practicum/shareit/config/JacksonConfig.java
+++ server/src/main/java/ru/practicum/shareit/config/JacksonConfig.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.config;
 
-import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.cfg.CoercionAction;
 import com.fasterxml.jackson.databind.cfg.CoercionInputShape;
